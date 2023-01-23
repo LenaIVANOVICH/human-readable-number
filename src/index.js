ba30cfe0bfb3e1d12715b94afa6bb23920437907
Lena Ivanovich <120718001+LenaIVANOVICH@users.noreply.github.com>
2023-01-23 15:25:14 +0300

    docs:add solution index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine','ten', 'eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+    if (0 <= number && number < 20){
+        return a[number];
+    }
 }
